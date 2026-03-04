2dbcb51f5eb3de2e02c40758aa85748ec3390789
madnordski <joe.king.phd@gmail.com>
2026-03-04 08:04:49 -0600

    Update site: Wed Mar  4 08:00:08 AM CST 2026

---
+++ Bikepacking/header.js
@@ -0,0 +1,44 @@
+var headerText = '<p align="center" class="noprint">'
+    + "<a href='/index.html'>Home</a> "
+    + " | <a href='index.html'>Joey's Bikepacking Page</a>"
+    + '<br><b>TD:</b> '
+    + '<a href="TourDivide2019.html">Day&nbsp;1:&nbsp;Signs of Bear Everywhere</a>'
+    + ' | <a href="td19d02.html">Day&nbsp;2:&nbsp;Koko Claims Another</a>'
+    + ' | <a href="td19d03.html">Day&nbsp;3:&nbsp;From Bumps to Bonks</a>'
+    + ' | <a href="td19d04.html">Day&nbsp;4:&nbsp;Eureka!</a>'
+    + ' | <a href="td19d05.html">Day&nbsp;5:&nbsp;Holland Lake Surprise</a>'
+    + ' | <a href="td19d06.html">Day&nbsp;6:&nbsp;A Day of Legends</a>'
+    + ' | <a href="td19d07.html">Day&nbsp;7:&nbsp;Returning to the Way West</a>'
+    + ' | <a href="td19d08.html">Day&nbsp;8:&nbsp;Buddies to Butte</a>'
+    + ' | <a href="td19d09.html">Day&nbsp;9:&nbsp;Montana High Country</a>'
+    + ' | <a href="td19d10.html">Day&nbsp;10:&nbsp;Rippin\' to Lima</a>'
+    + ' | <a href="td19d11.html">Day&nbsp;11:&nbsp;Idaho Ho</a>'
+    + ' | <a href="td19d12.html">Day&nbsp;12:&nbsp;A Grand Welcome to Wyoming</a>'
+    + ' | <a href="td19d13.html">Day&nbsp;13:&nbsp;Lava Mountain Madness</a>'
+    + ' | <a href="td19d14.html">Day&nbsp;14:&nbsp;Destination Pinedale</a>'
+    + ' | <a href="td19d15.html">Day&nbsp;15:&nbsp;Gambling on Atlantic City</a>'
+    + ' | <a href="td19d16.html">Day&nbsp;16 &amp; 17:&nbsp;The Great Divide Basin</a>'
+    + ' | <a href="td19bml.html">Brush Mountain Lodge</a>'
+    + ' | <a href="td19d18.html">Day&nbsp;18:&nbsp;Where You Ought To Be</a>'
+    + ' | <a href="td19d19.html">Day&nbsp;19:&nbsp;Thorny Situations</a>'
+    + ' | <a href="td19d20.html">Day&nbsp;20:&nbsp;Positively Salida</a>'
+    + ' | <a href="td19d21.html">Day&nbsp;21:&nbsp;Foul Occurrences</a>'
+    + ' | <a href="td19d22.html">Day&nbsp;22:&nbsp;Dry Norte</a>'
+    + ' | <a href="td19d23.html">Day&nbsp;23:&nbsp;Mythical Ice Cream</a>'
+    + ' | <a href="td19d24.html">Day&nbsp;24:&nbsp;In the Wilderness</a>'
+    + ' | <a href="td19d25.html">Day&nbsp;25:&nbsp;Saved by Sylvia</a>'
+    + ' | <a href="td19d26.html">Day&nbsp;26:&nbsp;Kindness &amp; Slime</a>'
+    + ' | <a href="td19d27.html">Day&nbsp;27:&nbsp;Pain &amp; Fisticuffs</a>'
+    + ' | <a href="td19d28.html">Day&nbsp;28:&nbsp;Ligtning Ride to Pie</a>'
+    + ' | <a href="td19d29.html">Day&nbsp;29:&nbsp;No Place for Bikes</a>'
+    + ' | <a href="td19d30.html">Day&nbsp;30:&nbsp;Jackrabbit Finish</a>'
+    + ' | <a href="PackingListBanff.pdf" target="_blank">Banff Packing List</a>'
+    + ' | <a href="PackingListPinedale.pdf"'
+    + '            target="_blank">Pinedale Packing List</a>'
+    + ' | <a href="slideshow.html" target="_blank">Slide Show</a>'
+    + '</p>';
+
+var title = 'Joey on the Tour Divide';
+
+var titleText = '<div class="page-break"></div><h1 align="center">' + title + '</h1>';
+
---
+++ Makefile
@@ -0,0 +1,12 @@
+push:
+	@git rev-parse --is-inside-work-tree >/dev/null
+	git add -A
+	git commit -m "Update site: $(shell date)" || true
+	@git show --name-status --oneline -1
+	git push origin main
+
+init:
+	git init
+	git branch -M main
+	-git remote add origin git@github.com:madnordski/joeyspage.github.io.git
+
